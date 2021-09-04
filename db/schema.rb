e6c379edde75b9ca2c711bbb40a7602ef9b186f3
晋也 <s.yoda0710@gmail.com>
2021-09-04 18:06:02 +0900

    マイグレーションを再実行

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_08_29_022958) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
